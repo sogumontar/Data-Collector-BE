bfe6fae9e63aaf9eb4a45533a7d743a1da04e501
Sogumontar Hendra Simangunsong <hendrasimz92@gmail.com>
2021-01-13 23:04:26 +0700

    trending API

--- app/Http/Controllers/TrendingController.php
+++ app/Http/Controllers/TrendingController.php
@@ -10,7 +10,12 @@ class TrendingController extends Controller
 {
     public function findAllTrending(){
         $users = DB::select('SELECT nomor, COUNT(nomor) AS jumlah FROM pesans GROUP BY nomor ORDER BY jumlah DESC LIMIT 10');
-        return $users;
+        $response = [
+            'code' => '200',
+            'status' => 'OK',
+            'data' => $users,
+        ];
+        return $response;
 
     }
 
