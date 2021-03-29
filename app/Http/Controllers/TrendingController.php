03f7f01c546937108a29822eea6c9041747628d0
Hendra Simangunsong <hendrasimz92@gmail.com>
2021-03-29 20:36:19 +0800

    fixing kategori at trending

--- app/Http/Controllers/TrendingController.php
+++ app/Http/Controllers/TrendingController.php
@@ -10,7 +10,7 @@ class TrendingController extends Controller
 {
     public function findAllTrending()
     {
-        $users = DB::select('SELECT nomor,kategori, isi, COUNT(nomor) AS jumlah FROM pesans GROUP BY nomor, isi ORDER BY jumlah DESC LIMIT 10');
+        $users = DB::select('SELECT nomor,kategori, isi, COUNT(nomor) AS jumlah FROM pesans GROUP BY nomor,kategori, isi ORDER BY jumlah DESC LIMIT 10');
         $response = [
             'code' => '200',
             'status' => 'OK',
